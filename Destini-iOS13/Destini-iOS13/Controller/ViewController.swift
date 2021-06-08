2620c12c1ce761a86a7a61dbb3cada9021047c4f
Tolerance <zpgy77@gmail.com>
2021-06-08 22:36:15 +0900

    Destini app

---
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    let stories: [Story] = [Story(title: "You see a fork in the road",
+                                  choice1: "Take a left",
+                                  choice2: "Take a right"),
+                            Story(title: "You see a tiger",
+                                                choice1: "Shout for help.",
+                                                choice2: "Play dead."),
+                            Story(title: "You find a treasure chest.",
+                                                choice1: "Open it.",
+                                                choice2: "Check for traps.")]
+    var index = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        storyLabel.text = stories[index].title
+        choice1Button.setTitle(stories[index].choice1, for: .normal)
+        choice2Button.setTitle(stories[index].choice2, for: .normal)
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
+}
+

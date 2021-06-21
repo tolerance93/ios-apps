462865809986c13a1df64b4092361bbbc7487e49
Tolerance <zpgy77@gmail.com>
2021-06-21 22:45:03 +0900

    Assignment

--- Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13/Tipsy/Controllers/CalculatorViewController.swift
@@ -16,14 +16,48 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var moneyPerPerson: String?
+    var tipPercentage: String?
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        tenPctButton.isSelected = false
+        zeroPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        billTextField.endEditing(true)
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
     }
     
     @IBAction func calculatePressed(_ sender: Any) {
+        var percent:Float = 0
+        if zeroPctButton.isSelected {
+            percent = 0
+            tipPercentage = "0"
+        } else if tenPctButton.isSelected {
+            percent = 0.1
+            tipPercentage = "10"
+        } else {
+            percent = 0.2
+            tipPercentage = "20"
+        }
+        let total = Float(billTextField.text ?? "0") ?? 0
+        let numOfPeople = Float(splitNumberLabel.text ?? "0") ?? 0
+        let calculatedTotal = (total * (1.0 + percent)) / numOfPeople
+        
+        moneyPerPerson = String(format: "%.2f", calculatedTotal)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.result = moneyPerPerson
+            destinationVC.numoberOfPeople = splitNumberLabel.text
+            destinationVC.tipPercentage = tipPercentage
+        }
     }
     
 }
--- Tipsy-iOS13/Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy-iOS13/Tipsy/Controllers/ResultsViewController.swift
@@ -13,24 +13,19 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
+    var result: String?
+    var numoberOfPeople: String?
+    var tipPercentage: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(numoberOfPeople ?? "0") people, with \(tipPercentage ?? "0")% tip."
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

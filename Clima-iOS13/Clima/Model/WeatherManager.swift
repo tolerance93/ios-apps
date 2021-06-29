b94dd0a17f4cf7daab35c69407ed145e832002a0
Tolerance <zpgy77@gmail.com>
2021-06-29 23:03:49 +0900

    Networking

---
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Tolerance on 2021/06/29.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e4d9b11cdefba5c2ba969fa75fea626e&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create URL
+        if let url = URL(string: urlString) {
+            //2. Create a URL Session
+            let session = URLSession(configuration: .default)
+            //3. Give the session the task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+}

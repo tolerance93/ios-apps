b18086340ad4c789bd4ae605f4fe52127eb1cae2
Tolerance <zpgy77@gmail.com>
2021-07-05 21:09:51 +0900

    JSON Decoding

---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Tolerance on 2021/07/05.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
